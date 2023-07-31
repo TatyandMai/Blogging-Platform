ad7593f05849ede35089fba77baab4c3a11f7d45
TatyandMai <139643600+TatyandMai@users.noreply.github.com>
2023-07-31 03:21:15 -0700

    Update postRoutes.js

--- Backend/Routes/postRoutes.js
+++ Backend/Routes/postRoutes.js
@@ -1,36 +1,45 @@
 const express = require("express");
 const router = express.Router();
-const postController = require("../Controllers/postController");
+// const postController = require("../Controllers/postController");
+
+const {
+    createPost,
+    deletePost,
+    getPost,
+    updatePost,
+    likeAndDislike,
+    getAllPosts,
+    getAllMyPosts,
+    getAllMyPostsProfile,
+    getMyAndFollowersPosts
+} = require("../Controllers/postController")
 
 // Route to create a new post
-router.post("/", postController.createPost);
+router.post("/", createPost);
 
 // Route to delete a post by its ID
-router.delete("/:postId", postController.deletePost);
+router.delete("/:postId", deletePost);
 
 // Route to get a post by its ID
-router.get("/:postId", postController.getPost);
+router.get("/:postId", getPost);
 
 // update a post by post Id
-router.put("/:id", postController.updatePost);
+router.put("/:id", updatePost);
 
 //like, dislike post
-router.put("/:id/like", postController, likeAndDislike);
-
-//get a post by post Id
-router.get("/:id", postController, getOnePost);
+router.put("/:id/like", likeAndDislike);
 
 //get all posts
-router.get("/api/v1/allPosts", postController, getAllPosts);
+router.get("/api/v1/allPosts", getAllPosts);
 
 //get all of my posts
-router.get("/api/v1/allMyPosts", postController, getAllMyPosts);
+router.get("/api/v1/allMyPosts", getAllMyPosts);
 
 //get all of my posts in profile page
-router.get("/profile/:username", postController, getAllMyPostsProfile);
+router.get("/profile/:username", getAllMyPostsProfile);
 
 //get all of following people's posts and my posts
-router.get("/timeline/:userId", postController, getMyAndFollowersPosts);
+router.get("/timeline/:userId", getMyAndFollowersPosts);
 
 
 module.exports = router;
